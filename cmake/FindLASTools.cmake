315a1457b980fd3dfdc6e789a0558835bc7f4d44
Wukun <wk707060335@gmail.com>
2019-05-15 16:48:55 +0800

    lastools

---
+++ cmake/FindLASTools.cmake
@@ -0,0 +1,10 @@
+include(ExternalProject)
+
+ExternalProject_Add(LASTools
+        GIT_REPOSITORY https://github.com/LAStools/LAStools.git
+        CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${EXTERNAL_INSTALL_LOCATION}
+        )
+include_directories(
+        ${EXTERNAL_INSTALL_LOCATION}/include/LASlib)
+link_directories(
+        ${EXTERNAL_INSTALL_LOCATION}/lib/LASlib)
